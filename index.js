4071ca10d8d1ddc1e73cdb6ada64346b94f57026
madhuripani <taddimadhuri970@gmail.com>
2021-11-11 19:25:07 +0530

    forms

--- index.js
+++ index.js
@@ -1,48 +1,25 @@
-/* ------------------------------------ Click on login and Sign Up to  changue and view the effect
----------------------------------------
-*/
+$(document).ready(function() {
 
-function cambiar_login() {
-    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
-  document.querySelector('.cont_form_login').style.display = "block";
-  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
-  
-  setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);  
-    
-  setTimeout(function(){    
-  document.querySelector('.cont_form_sign_up').style.display = "none";
-  },200);  
-    }
-  
-  function cambiar_sign_up(at) {
-    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
-    document.querySelector('.cont_form_sign_up').style.display = "block";
-  document.querySelector('.cont_form_login').style.opacity = "0";
-    
-  setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
-  },100);  
-  
-  setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
-  },400);  
-  
-  
-  }    
-  
-  
-  
-  function ocultar_login_sign_up() {
-  
-  document.querySelector('.cont_forms').className = "cont_forms";  
-  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
-  document.querySelector('.cont_form_login').style.opacity = "0"; 
-  
-  setTimeout(function(){
-  document.querySelector('.cont_form_sign_up').style.display = "none";
-  document.querySelector('.cont_form_login').style.display = "none";
-  },500);  
-    
-    }
-  
-  
-  
-  
+  var state = false;
+
+  //$("input:text:visible:first").focus();
+
+  $('#accesspanel').on('submit', function(e) {
+
+      e.preventDefault();
+
+      state = !state;
+
+      if (state) {
+          document.getElementById("litheader").className = "poweron";
+          document.getElementById("go").className = "";
+          document.getElementById("go").value = "Initializing...";
+      }else{
+          document.getElementById("litheader").className = "";
+          document.getElementById("go").className = "denied";
+          document.getElementById("go").value = "Access Denied";
+      }
+
+  });
+
+});
